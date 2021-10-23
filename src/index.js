42865b2f75e109f2903efe3bbf8b4cdee2cf6a29
JoGorska <83362036+JoGorska@users.noreply.github.com>
2021-10-23 10:57:53 +0100

    Removed app.js and used index.js to render ul onto the page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ul>
+    <li>first list item</li>
+    <li>second list item</li>
+    <li>third list item</li>
+  </ul>,
   document.getElementById('root')
 );
 
