77be8f6f4d76b6e048f026fd46603316342fb1ec
JoGorska <83362036+JoGorska@users.noreply.github.com>
2021-10-26 21:42:18 +0100

    Add modal to open when user wants to add a new task. Placeholder text before form is added in it's place

--- src/components/NewButton.js
+++ src/components/NewButton.js
@@ -4,7 +4,38 @@ import React from "react";
 
 function NewButton(props) {
     return (
-        <button onClick={props.addNewTask()}>Add New Task</button>
+        <div>
+            {//Button triggering Modal
+            }
+            <button type="button" className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Add New Task
+            </button>
+
+            {// Modal from Bootstrap
+            }
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">New Task</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                    need to add id automaticaly
+                    need to add text
+                    need to add completed property false
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button onClick={props.addNewTask()} type="button" className="btn btn-primary">Save changes</button>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+
+
+        // <button onClick={props.addNewTask()} className="btn btn-outline-secondary">Add New Task</button>
     )
 }
 
