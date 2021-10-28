db64cdd3f2fe62e946fce9169e30a7c69cb8ace2
JoGorska <83362036+JoGorska@users.noreply.github.com>
2021-10-28 17:53:38 +0100

    Fix indentation in New Button and add more comments and removed unusused component NewTaskForm

--- src/components/NewButton.js
+++ src/components/NewButton.js
@@ -1,57 +1,55 @@
 import React from "react";
-import FormNewTask from "./FormNewTask"
+// import FormNewTask from "./FormNewTask"
 
 
 
 function NewButton(props) {
     return (
         <div>
-            {//Button triggering Modal
-            }
+            {/* Button triggering Modal */}
+            
             <button type="button" className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">
             Add New Task
             </button>
 
-            {// Modal from Bootstrap
-            }
+            {/* Modal from Bootstrap */}
+            
             <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                <div className="modal-header">
-                    <h5 className="modal-title" id="exampleModalLabel">New Task</h5>
-                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div className="modal-body">
-
-                {/* Form starts here */}
-
-                <form onSubmit={props.onSubmit}>
-                    
-                    <div className="mb-3">
-                        <label className="form-label" htmlFor="text">Write the task here</label>
-                        <input 
-                            name="text" 
-                            // value={this.state.text} 
-                            // onChange={props.handleChangeForm} 
-                            className="form-control"
-                            id="text"
-                        />
-
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">New Task</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
 
-                {/* Form's submit button. ??? might need to move it? */}
-                    <button type="submit"  >Submit</button>
-                </form>
-                {/* Form ends here */}
-                </div>
+                    {/* Form starts here */}
+                    <form onSubmit={props.onSubmit}>
+                        <div className="modal-body">
 
-                {/* Buttons build in the modal ??? might need to remove ??? */}
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button onClick={props.addNewTask} type="button" className="btn btn-primary">Save changes</button>
+                            <div className="mb-3">
+                                <label className="form-label" htmlFor="text">Write the task here</label>
+                                <input 
+                                    name="text" 
+                                    // Do I need to handle on change at all at this stage???
+                                    // value={this.state.text} 
+                                    // onChange={props.handleChangeForm} 
+                                    className="form-control"
+                                    id="text"
+                                />
+
+                            </div>
+                        
+                        </div>
+
+                        {/* Buttons */}
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button  type="submit" className="btn btn-primary">Save New Task</button>
+                        </div>
+                    </form>
+                    {/* Form ends here */}
+                    </div>
                 </div>
-                </div>
-            </div>
             </div>
         </div>
 
